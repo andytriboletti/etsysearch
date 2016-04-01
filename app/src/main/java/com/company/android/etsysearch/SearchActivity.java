aab3db0c39b13b95591fc36cc1fb1fd4b8e2923f
Andy Triboletti <andy.triboletti@gmail.com>
2016-03-31 20:34:20 -0400

    fix for searching more than once

--- app/src/main/java/com/company/android/etsysearch/SearchActivity.java
+++ app/src/main/java/com/company/android/etsysearch/SearchActivity.java
@@ -50,6 +50,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Timber.d(query);
+                adapter=null;
                 loadPosts(query);
                 if( ! searchView.isIconified()) {
 
